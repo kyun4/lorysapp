465886067a8059a17f8e05bf974af59333965d3a
kyun04 <efrencamposagrado0622@gmail.com>
2024-09-12 14:20:06 +0800

    livestream features only

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,10 +20,11 @@ class _HomePageState extends State<HomePage> {
 
     _controller = VideoPlayerController.network(
         "https://firebasestorage.googleapis.com/v0/b/lorys-app.appspot.com/o/fyp%2Fchezka1.mp4?alt=media&token=366053c9-3397-4ee0-a4df-ceb9c52ca2ce");
-
+    // https://labex-wfuoqqcqxo.oss-us-west-1.aliyuncs.com/labex-imgs/y2mate.com%20-%20%23%ED%95%9C%EC%9D%BC%ED%86%B1%ED%85%90%EC%87%BC%20%23%EC%B9%B4%EB%85%B8%EC%9A%B0%EB%AF%B8%EC%9C%A0%20%23%EC%A7%80%EC%A7%80%EB%A7%90%EC%95%84%EC%9A%94%5B7405452225759235335-h264_540p_611078-0%5D.mp4
     _initializeVideoPlayerFuture = _controller.initialize();
     _controller.setLooping(true);
     _controller.play();
+    _controller.initialize().then((_) => {setState(() {})});
   }
 
   @override
@@ -56,13 +57,11 @@ class _HomePageState extends State<HomePage> {
             child: FutureBuilder(
                 future: _initializeVideoPlayerFuture,
                 builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return AspectRatio(
-                        aspectRatio: _controller.value.aspectRatio,
-                        child: VideoPlayer(_controller));
-                  } else {
-                    return Center(child: CircularProgressIndicator());
-                  }
+                  return _controller.value.isInitialized
+                      ? AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller))
+                      : Center(child: CircularProgressIndicator());
                 })));
   }
 }
